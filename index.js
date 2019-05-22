ba26673825b3860073bd70d0c3a65be21ae237f8
Chris Lord <chrislord.net@gmail.com>
2019-05-22 13:45:06 +0100

    Split out core functionality from index.js + add command-line parameters
    index.js now handles the express server bits and shortener.js handles
    database access and responses. Would be cleaner to handle responses in
    index.js too really, but this will do.
    
    This should make it easier to do unit testing.

--- index.js
+++ index.js
@@ -1,126 +1,15 @@
+const bodyParser = require('body-parser');
 const exp = require('express');
 const exphbs = require('express-handlebars');
-const bodyParser = require('body-parser');
-const mongo = require('mongodb').MongoClient;
+const minimist = require('minimist');
 
-const dburl = 'mongodb://localhost/url-shortener';
-const port = 8080;
 const app = exp();
 
-function dbConnect(response, callback)
-{
-    mongo.connect(dburl, async (err, client) => {
-        if (err) {
-            response.render('home', {
-                'error-message': 'Database error: ' + err
-            });
-            return;
-        }
+var argv = minimist(process.argv.slice(2));
+var dburl = argv.d ? argv.d : 'mongodb://localhost/url-shortener';
+var port = (argv.p && argv.p > 0) ? argv.p : 8080;
 
-        callback(client);
-    });
-}
-
-// Shorten a URL and store it under @code. If @code is empty, create a new, random key.
-function shorten(url, code, response)
-{
-    // Validate code
-    if (code != '') {
-        if (code.length < 4 || code.length > 16 || code == 'shorten' || code == 'view') {
-            response.render('home', {
-                'error-message': 'Invalid URL code'
-            });
-            return;
-        }
-    }
-
-    dbConnect(response, async (client) => {
-        var db = client.db();
-        var count;
-        if (code == '') {
-            // If no code specified, create a new random code
-            do {
-                // Note, this is only pseudo-random, but should be fine for this
-                // purpose. It won't generate upper-case letters either, but I like it
-                // for compactness (stolen from stackoverflow of course...)
-                code = (Math.random()+1).toString(36).substr(2,4);
-
-                // If we wanted to be really safe, we'd count how many times this
-                // evaluates to true and have some kind of limit.
-                count = await db.collection('URLs').countDocuments({'code': code});
-            } while(count > 0);
-        } else {
-            // Check to see that this code doesn't already exist
-            count = await db.collection('URLs').countDocuments({'code': code});
-            if (count > 0) {
-                client.close();
-                response.render('home', {
-                    'error-message': 'Code already exists'
-                });
-                return;
-            }
-        }
-
-        // Insert new shortened URL record
-        db.collection('URLs').insertOne({
-            'code': code,
-            'url': url
-        });
-        client.close();
-
-        // Display success page
-        response.render('result', {
-            "url": url,
-            "short-url": code
-        });
-    });
-}
-
-// Redirect to a URL stored under @code
-function retrieve(code, response, redirect)
-{
-    dbConnect(response, (client) => {
-        var db = client.db();
-        db.collection('URLs').findOne({'code': code}).then((result) => {
-            if (!result) {
-                console.log(`Record '${code}' not found`);
-                response.render('home', {
-                    'error-message': 'Code not found'
-                });
-            } else {
-                console.log('Found record', result);
-                if (redirect) {
-                    // Redirect to shortened URL
-                    response.redirect(301, result.url);
-                } else {
-                    // Display shortened URL
-                    response.render('result', {
-                        "url": result.url,
-                        "short-url": code
-                    });            
-                }
-            }
-            client.close();
-        });
-    });
-}
-
-// View all records, sorted by most recent first
-function view(response)
-{
-    // Paging here would be nice, as it is, limit viewing to 100 records
-    dbConnect(response, (client) => {
-        var db = client.db();
-        db.collection('URLs').find({}).sort({$natural:-1}).limit(100).toArray().then((result) => {
-            var count = result ? result.length : 0;
-            response.render('view', {
-                "count": count,
-                "records": result
-            });
-            client.close();
-        });
-    });
-}
+const shortener = require('./shortener')(dburl);
 
 // Setup handlebars
 app.engine('.hbs', exphbs({
@@ -146,23 +35,23 @@ app.get('/', (request, response) => {
 
 app.get('/view', (request, response) => {
     console.log('Retrieving view page');
-    view(response);
+    shortener.view(response);
 });
 
 // Respond to shorten POST
 app.post('/shorten', (request, response) => {
     console.log(`Shortening '${request.body.url}' to '${request.body.code}'`);
     if (request.body.url == '') {
-        retrieve(request.body.code, response, false);
+        shortener.retrieve(request.body.code, response, false);
     } else {
-        shorten(request.body.url, request.body.code, response);
+        shortener.shorten(request.body.url, request.body.code, response);
     }
 });
 
 // Respond to random requests
 app.get('/*', (request, response) => {
     console.log(`Retrieving URL '${request.originalUrl}'`);
-    retrieve(request.originalUrl.substr(1), response, true);
+    shortener.retrieve(request.originalUrl.substr(1), response, true);
 })
 
 // Exception reporting
@@ -173,5 +62,5 @@ app.use((err, request, response, next) => {
 })
 
 // Start
-console.log(`Starting server on port ${port}`);
+console.log(`Starting server on port ${port} with database '${dburl}'`);
 app.listen(port);
---
+++ shortener.js
@@ -0,0 +1,122 @@
+const mongo = require('mongodb').MongoClient;
+
+module.exports = function(dburl) {
+    var module = {};
+
+    module.dbConnect = function(response, callback)
+    {
+        mongo.connect(dburl, async (err, client) => {
+            if (err) {
+                response.render('home', {
+                    'error-message': 'Database error: ' + err
+                });
+                return;
+            }
+
+            callback(client);
+        });
+    }
+
+    // Shorten a URL and store it under @code. If @code is empty, create a new, random key.
+    module.shorten = function(url, code, response)
+    {
+        // Validate code
+        if (code != '') {
+            if (code.length < 4 || code.length > 16 || code == 'shorten' || code == 'view') {
+                response.render('home', {
+                    'error-message': 'Invalid URL code'
+                });
+                return;
+            }
+        }
+
+        this.dbConnect(response, async (client) => {
+            var db = client.db();
+            var count;
+            if (code == '') {
+                // If no code specified, create a new random code
+                do {
+                    // Note, this is only pseudo-random, but should be fine for this
+                    // purpose. It won't generate upper-case letters either, but I like it
+                    // for compactness (stolen from stackoverflow of course...)
+                    code = (Math.random()+1).toString(36).substr(2,4);
+
+                    // If we wanted to be really safe, we'd count how many times this
+                    // evaluates to true and have some kind of limit.
+                    count = await db.collection('URLs').countDocuments({'code': code});
+                } while(count > 0);
+            } else {
+                // Check to see that this code doesn't already exist
+                count = await db.collection('URLs').countDocuments({'code': code});
+                if (count > 0) {
+                    client.close();
+                    response.render('home', {
+                        'error-message': 'Code already exists'
+                    });
+                    return;
+                }
+            }
+
+            // Insert new shortened URL record
+            db.collection('URLs').insertOne({
+                'code': code,
+                'url': url
+            });
+            client.close();
+
+            // Display success page
+            response.render('result', {
+                "url": url,
+                "short-url": code
+            });
+        });
+    }
+
+    // Redirect to a URL stored under @code
+    module.retrieve = function(code, response, redirect)
+    {
+        this.dbConnect(response, (client) => {
+            var db = client.db();
+            db.collection('URLs').findOne({'code': code}).then((result) => {
+                if (!result) {
+                    console.log(`Record '${code}' not found`);
+                    response.render('home', {
+                        'error-message': 'Code not found'
+                    });
+                } else {
+                    console.log('Found record', result);
+                    if (redirect) {
+                        // Redirect to shortened URL
+                        response.redirect(301, result.url);
+                    } else {
+                        // Display shortened URL
+                        response.render('result', {
+                            "url": result.url,
+                            "short-url": code
+                        });            
+                    }
+                }
+                client.close();
+            });
+        });
+    }
+
+    // View all records, sorted by most recent first
+    module.view = function(response)
+    {
+        // Paging here would be nice, as it is, limit viewing to 100 records
+        this.dbConnect(response, (client) => {
+            var db = client.db();
+            db.collection('URLs').find({}).sort({$natural:-1}).limit(100).toArray().then((result) => {
+                var count = result ? result.length : 0;
+                response.render('view', {
+                    "count": count,
+                    "records": result
+                });
+                client.close();
+            });
+        });
+    }
+
+    return module;
+};
